16eb1dc232a1bad5093108dc2ca0e53ade42b661
ezesilva95 <4381@holbertonschool.com>
2022-04-26 08:24:35 -0700

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for firstn in range(0, 10):
     for lastn in range((firstn+1), 10):
         if (firstn != 8):
-            print(f"{firstn}{lastn}, ", end="")
+            print("{}{}, ".format(firstn, lastn), end="")
         else:
-            print(f"{firstn}{lastn}")
+            print("{}{}".format(firstn, lastn))
