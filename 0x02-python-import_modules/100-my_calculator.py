5875de4310871fb9fdaff8c0c1decdcc0289277e
ezesilva95 <4381@holbertonschool.com>
2022-04-28 17:05:04 -0700

    Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 from sys import argv
+
 if __name__ == "__main__":
     if len(argv) != 4:
-            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-            exit(1)
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
     op = argv[2]
-    operator = {"+": add, "-": sub, "*": mul, "/": div}
-    if operator not in op:
+    operator = {"+", "-", "*", "/"}
+    if op not in operator:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
-    print(f"{a} {operator} {b} = {op[operator](a, b)}")
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] == '+':
+            print(f"{a} {op} {b} = {add(a, b)}")
+        elif argv[2] == '-':
+            print(f"{a} {op} {b} = {sub(a, b)}")
+        elif argv[2] == '*':
+            print(f"{a} {op} {b} = {mul(a, b)}")
+        else:
+            print(f"{a} {op} {b} = {div(a, b)}")
