bb879ca633efc0f95775fcc13cea6ae422f5917d
ezesilva95 <4381@holbertonschool.com>
2022-05-02 10:48:29 -0700

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 def multiple_returns(sentence):
     length = len(sentence)
-    if sentence[0]:
+    if length > 0:
         first_char = sentence[0]
     else:
         first_char = None
