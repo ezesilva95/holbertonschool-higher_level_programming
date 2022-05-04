153dbf73a386cd71f8855aa3e403d20be7c5aaa5
ezesilva95 <4381@holbertonschool.com>
2022-05-03 17:35:15 -0700

    Task 14

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -14,7 +14,7 @@ void print_python_list_info(PyObject *p)
 	int i = 0;
 
 	printf("[*] Size of the Python List = %d\n", size);
-	printf("[*] Allocated = %d\n", alloc); 
+	printf("[*] Allocated = %d\n", alloc);
 	for (i = 0; i < size; i++)
 	{
 		printf("Element %ld: %s\n",
