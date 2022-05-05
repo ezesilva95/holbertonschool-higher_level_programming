29a2260a2bd178204db18c26e461a64448b1bcf6
ezesilva95 <4381@holbertonschool.com>
2022-05-04 17:58:49 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    numr = 0
     if roman_string is None and type(roman_string) is not str:
         return (0)
     else:
-        for i in range(len(roman_string)):
-            roman_string += "-"
-            next_i = roman_string[i + 1]
-            if roman_string[i] == 'I':
-                if next_i == 'V' or next_i == 'X':
-                    num -= 1
+        num = 0
+        for i, c in enumerate(roman_string):
+            next_i = i+ 1
+            if c == 'I':
+                if next_i < len(roman_string):
+                    if next_i == 'V' or next_i == 'X':
+                        num -= 1
+                    else:
+                        num += 1
                 else:
                     num += 1
-            if roman_string[i] == 'V':
+            if c == 'V':
                 num += 5
-            if roman_string[i] == 'X':
+            if c == 'X':
                 if next_i == 'L' or next_i == 'C':
                     num -= 10
                 else:
                     num += 10
-            if roman_string[i] == 'L':
+            if c == 'L':
                 num += 50
-            if roman_string[i] == 'C':
+            if c == 'C':
                 if next_i == 'D' or next_i == 'M':
                     num -= 100
                 else:
                     num += 100
-            if roman_string[i] == 'D':
+            if c == 'D':
                 if next_i == 'M':
                     num -= 500
                 else:
                     num += 500
-            if roman_string[i] == 'M':
+            if c == 'M':
                 num += 1000
+            roman_string = roman_string[:-1]
         return num
     return()
