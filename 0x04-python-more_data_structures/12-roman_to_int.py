9f2908107e13ab5ac4ee0ca3761b73d4df8f6668
ezesilva95 <4381@holbertonschool.com>
2022-05-04 18:32:15 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,39 +2,30 @@
 def roman_to_int(roman_string):
     if roman_string is not None and type(roman_string) is str:
         num = 0
-        for i, c in enumerate(roman_string):
+        roman_string += "-"
+        for i in range(len((roman_string)) - 1):
             j = i + 1
-            if c == 'I':
-                if j < len(roman_string):
-                    if roman_string[j] == 'V' or roman_string[j] == 'X':
-                        num -= 1
-                    else:
-                        num += 1
-                else:
-                    num += 1
-            if c == 'V':
+            if roman_string[i] == 'I':
+                if roman_string[j] == 'V' or roman_string[j] == 'X':
+                    num -= 1
+                num += 1
+            if roman_string[i] == 'V':
                 num += 5
-            if c == 'X':
-                if j < len(roman_string):
-                    if roman_string[j] == 'L' or roman_string[j] == 'C':
-                        num -= 10
-                    else:
-                        num += 10
+            if roman_string[i] == 'X':
+                if roman_string[j] == 'L' or roman_string[j] == 'C':
+                    num -= 10
                 else:
                     num += 10
-            if c == 'L':
+            if roman_string[i] == 'L':
                 num += 50
-            if c == 'C':
-                if j < len(roman_string):
-                    if roman_string[j] == 'D' or roman_string[j] == 'M':
-                        num -= 100
-                    else:
-                        num += 100
+            if roman_string[i] == 'C':
+                if roman_string[j] == 'D' or roman_string[j] == 'M':
+                    num -= 100
                 else:
                     num += 100
-            if c == 'D':
+            if roman_string[i] == 'D':
                 num += 500
-            if c == 'M':
+            if roman_string[i] == 'M':
                 num += 1000
         return num
     else:
