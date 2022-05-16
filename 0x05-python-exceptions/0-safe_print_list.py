6d59b46b6cc703584723ed3846919d0b392b1c83
ezesilva95 <4381@holbertonschool.com>
2022-05-16 08:08:32 -0700

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     while i < x:
         try:
             print(f"{my_list[i]}", end="")
-        except:
+        except IndexError:
             break
         i += 1
     print("")
