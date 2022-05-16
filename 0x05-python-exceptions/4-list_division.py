715ce42f95011fe7d730aa9a2e6d73ee958bd6d2
ezesilva95 <4381@holbertonschool.com>
2022-05-16 10:12:47 -0700

    Task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,7 +10,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except (TypeError):
             print("wrong type")
         except (IndexError):
-            print("out of error")
+            print("out of range")
         finally:
             new_list.append(div)
-    return new_list          
+    return new_list
