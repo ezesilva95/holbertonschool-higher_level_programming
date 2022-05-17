f1d46ab72f0726f4f4e3340a3b8580715e094ded
ezesilva95 <4381@holbertonschool.com>
2022-05-17 13:05:50 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+class Square that defines a square by: (based on 0-square.py)
+'''
+
+
+class Square:
+    '''
+    class square
+    '''
+    def __init__(self, size):
+        '''
+        init square
+
+        The size of a square is crucial for a square.
+        One way to have the control is to keep it privately. 
+        You will see in next tasks how to get, 
+        update and validate the size value.
+        '''
+        self.__size = size
