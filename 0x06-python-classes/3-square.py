69696ab484617647d9db6d447868dc34bb826729
ezesilva95 <4381@holbertonschool.com>
2022-05-17 13:53:45 -0700

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+class Square that defines a square by: (based on 2-square.py)
+'''
+
+
+class Square:
+    '''
+    class square
+    '''
+
+    def __init__(self, size=0):
+        '''
+        init square
+
+        The size of a square is crucial for a square.
+        One way to have the control is to keep it privately.
+        You will see in next tasks how to get,
+        update and validate the size value.
+        '''
+        if type(size) is int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        '''
+        calculates area of the square
+        '''
+        
+        return (self.__size)**2
