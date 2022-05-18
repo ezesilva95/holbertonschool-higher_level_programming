37d502d90c1b8fe78a2b38843af7d479c1eee349
ezesilva95 <4381@holbertonschool.com>
2022-05-18 11:20:47 -0700

    Task 6

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+'''
+class Rectangle that defines a rectangle by: (based on 5-rectangle.py)
+'''
+
+
+class Rectangle:
+    '''
+    class rectangle
+    '''
+
+    number_of_instance = 0
+
+    def __init__(self, width=0, height=0):
+        '''
+        initilizate rectangles
+        '''
+        self.height = height
+        self.width = width
+        Rectangle.number_of_instance += 1
+
+    @property
+    def width(self):
+        '''
+        get widht
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''
+        set width
+        '''
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+        else:
+            raise TypeError("width must be an integer")
+
+    @property
+    def height(self):
+        '''
+        get height
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''
+        set height
+        '''
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__height = value
+        else:
+            raise TypeError("width must be an integer")
+
+    def area(self):
+        '''
+        return area of rectangle
+        '''
+        return self.__width * self.__height
+
+    def perimeter(self):
+        '''
+        return perimeter
+        '''
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (2 * self.__width) + (2 * self.__height)
+
+    def __str__(self):
+        '''
+        print rectangle with #
+        '''
+        if self.__width == 0 or self.height == 0:
+            return ""
+        pic = "\n".join(["#" * self.__width for rows in range(self.__height)])
+        return pic
+
+    def __repr__(self):
+        '''
+        str representation
+        '''
+        return (f"Rectangle({self.width:d}, {self.height:d})")
+    
+    def __del__(self):
+        '''
+        delete instance class
+        '''
+        print("Bye rectangle...")
+        Rectangle.number_of_instance -= 1
