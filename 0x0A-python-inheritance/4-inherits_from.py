8fc4591907c8f5f422b9724c97587e50617b2d35
ezesilva95 <4381@holbertonschool.com>
2022-05-30 14:19:49 -0700

    Task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 '''
 4. Only sub class of
-function that returns True if the object is an instance of a class
-that inherited (directly or indirectly) from the specified class
 '''
 
 
