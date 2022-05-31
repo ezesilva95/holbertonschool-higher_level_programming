873de7202ee452438bc5315945861b1110643f89
ezesilva95 <4381@holbertonschool.com>
2022-05-31 16:42:52 -0700

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,7 +5,6 @@ Module 7. Load, add, save
 
 
 from sys import argv
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -14,6 +13,6 @@ f = "add_item.json"
 try:
     data = load_from_json_file(f)
 except FileNotFoundError:
-    load = []
+    data = []
 
 save_to_json_file(data + argv[1:], f)
