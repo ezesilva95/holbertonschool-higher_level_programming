ed503b62eb72c93f3204413916967cad7fe2ed58
ezesilva95 <4381@holbertonschool.com>
2022-05-31 16:16:19 -0700

    Task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,12 +9,12 @@ class Student():
     defines a student by, first name, last name, age
     '''
     def __init__(self, first_name, last_name, age):
-            '''
-            Instantiation with first_name, last_name and age
-            '''
-            self.first_name = first_name
-            self.last_name = last_name
-            self.age = age
+        '''
+        Instantiation with first_name, last_name and age
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         '''
