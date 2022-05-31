ea1b3b0f30627c6d8f21e5345bb543f326ce09fc
ezesilva95 <4381@holbertonschool.com>
2022-05-31 09:16:33 -0700

    Task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+Module 9. Student to JSON
+'''
+
+
+class Student():
+    '''
+    defines a student by, first name, last name, age
+    '''
+    def __init__(self, first_name, last_name, age):
+            '''
+            Instantiation with first_name, last_name and age
+            '''
+            self.first_name = first_name
+            self.last_name = last_name
+            self.age = age
+
+    def to_json(self):
+        '''
+        retrieves a dictionary representation of a Student instance
+        '''
+        return self.__dict__
