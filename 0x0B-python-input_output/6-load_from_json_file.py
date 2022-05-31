6bc1a334abb15fd672672d0042794777beab06ed
ezesilva95 <4381@holbertonschool.com>
2022-05-31 09:15:42 -0700

    Task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Module 6. Create object from a JSON file
+'''
+import json
+
+
+def load_from_json_file(filename):
+    '''
+    creates an Object from a “JSON file”
+    '''
+    with open(filename, mode='r', encoding="utf-8") as f:
+        json.load(f)
