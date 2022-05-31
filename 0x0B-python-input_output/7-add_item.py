d4af485674796b7b738b8ae93a36f6287379e30f
ezesilva95 <4381@holbertonschool.com>
2022-05-31 16:14:54 -0700

    Task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+7. Load, add, save
+'''
+
+from sys import argv
+save_to_json_file = __import__('5t-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+f = "add_item.json"
+
+try:
+    data = load_from_json_file(f)
+except:
+    data = []
+
+save_to_json_file(data + argv[1:], f)
