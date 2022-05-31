5e0cc27fc5649d151d09c54357d00da61e847b37
ezesilva95 <4381@holbertonschool.com>
2022-05-31 09:16:56 -0700

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+10. Student to JSON with filter
+'''
+
+
+class Student():
+    '''
+    defines a student
+    '''
+    def __init__(self, first_name, last_name, age):
+        '''
+        Instantiation with first_name, last_name and age
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        retrieves a dictionary representation of a Student instance 
+        '''
+        if attrs is None:
+            return self.__dict__
+        else:
+            dictionary = {}
+            for at in attrs:
+                if at in self.__dict__.keys():
+                    dictionary[at] = self.__dict__[at]
+            return dictionary
