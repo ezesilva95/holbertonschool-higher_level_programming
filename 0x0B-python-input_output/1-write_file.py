5be631059511551707b2f4f3cf99b24a255967f8
ezesilva95 <4381@holbertonschool.com>
2022-05-31 08:26:51 -0700

    Task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+'''
+Module 3-write_file
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    writes to text file(UTF8) and returns num chars written
+    '''
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return(f.write(text))
