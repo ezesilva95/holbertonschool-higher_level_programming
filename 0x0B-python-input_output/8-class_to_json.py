de252522d7fb7721a4615049e3f88eb5e020ca8b
ezesilva95 <4381@holbertonschool.com>
2022-05-31 09:16:11 -0700

    Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+8-class_to_json.py
+'''
+
+
+def class_to_json(obj):
+    '''
+    returns the dictionary description with simple data structure
+    '''
+    return obj.__dict__
