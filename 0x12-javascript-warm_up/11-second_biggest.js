9edeaeb730d5a5f4ca80ed6ad1145251934ce34c
ezesilva95 <4381@holbertonschool.com>
2022-08-01 16:16:10 -0700

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const arry = process.argv.slice(2);
 function SecndMax (array) {
   if (array.length < 2) {
-    return(0);
+    return (0);
   } else {
     array.sort((x, y) => x - y);
     array.pop();
