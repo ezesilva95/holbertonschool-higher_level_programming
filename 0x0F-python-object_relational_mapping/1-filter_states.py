8d1a8d1cd7eb4dcf9da840882b7e5d55f36c7345
ezesilva95 <4381@holbertonschool.com>
2022-08-07 18:07:09 -0700

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
@@ -9,10 +10,11 @@ from sys import argv
 if __name__ == "__main__":
 
     db = MySQLdb.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2],
-                            db=argv[3])
+                         user=argv[1], passwd=argv[2],
+                         db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name REGEXP '^[n]' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name REGEXP '^[n]'
+                ORDER BY id ASC")
     for row in cur.fetchall():
         print(row)
     cur.close()
