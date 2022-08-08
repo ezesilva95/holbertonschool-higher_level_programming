7947165f4ec412b4958060059858700fa7c6a614
ezesilva95 <4381@holbertonschool.com>
2022-08-07 18:12:33 -0700

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument
+script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument
 But this time, write one that is safe from MySQL injections!
 """
 
@@ -8,10 +9,10 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    
+
     db = MySQLdb.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2],
-                            db=argv[3])
+                         user=argv[1], passwd=argv[2],
+                         db=argv[3])
 
     cur = db.cursor()
     query = """SELECT * FROM states wHERE name=%s ORDER BY id ASC"""
