fd7b2742dffd00c1ab3beda92777f4a95adf5ba6
ezesilva95 <4381@holbertonschool.com>
2022-08-18 18:59:09 -0700

    Task 8

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #8. Catch me if you can!
-curl -sLX PUT 0.0.0.0:5000/catch_me "user_id=98" -H "Origin: HolbertonSchool"
+curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
