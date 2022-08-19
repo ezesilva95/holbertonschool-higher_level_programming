84014914e515a8b681f60547e7e4f6ef4b7fd6f1
ezesilva95 <4381@holbertonschool.com>
2022-08-19 09:22:23 -0700

    Task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,6 +11,6 @@ import requests
 if __name__ == "__main__":
     r = requests.get(argv[1])
     if r.status_code >= 400:
-      print("Error code: {}".format(r.status_code))
+        print("Error code: {}".format(r.status_code))
     else:
-      print(r.text) 
+        print(r.text)
