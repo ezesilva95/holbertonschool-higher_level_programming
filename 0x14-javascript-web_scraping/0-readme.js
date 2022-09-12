a73b41e88d0546af4118f4423f4dbf7432d62508
ezesilva95 <4381@holbertonstudents.com>
2022-09-12 15:39:24 +0000

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const _file = process.argv[2];
 
-fs.readFile(_file, 'utf-8', (err, data){
+fs.readFile(_file, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
