16e19bc69ee4486bb0f3029edb5aa1cccc60e088
ezesilva95 <4381@holbertonstudents.com>
2022-09-12 21:03:10 +0000

    Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,12 +3,12 @@
 const axios = require('axios');
 const fs = require('fs');
 const url = process.argv[2];
-const file_path = process.argv[3];
+const filepath = process.argv[3];
 
 axios.get(url)
   .then(response => {
     const content = response.data;
-    fs.writeFile(file_path, content, err => {
+    fs.writeFile(filepath, content, err => {
       if (err) {
         console.error(err);
       }
