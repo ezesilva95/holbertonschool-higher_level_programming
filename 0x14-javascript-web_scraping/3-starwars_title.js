ae0cc24ad79f9208e68666eb20dc357cbe9fd558
ezesilva95 <4381@holbertonstudents.com>
2022-09-12 16:51:13 +0000

    Task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+const episode  = process.argv[2];
+
+axios.get(`https://swapi-api.hbtn.io/api/films/${episode}`)
+  .then(response => {
+    console.log(response.data.title);
+  })
+  .catch(error => {
+    console.log(error.response.status);
+  });
